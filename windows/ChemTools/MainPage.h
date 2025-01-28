35db8164421a3f3dcb047a2143f9f246ea1a2b0b
kaipol <1335791724@qq.com>
2025-01-28 15:24:14 +0800

    first commit

---
+++ windows/ChemTools/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::ChemTools::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::ChemTools::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
